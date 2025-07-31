9f5ab094033bed9fc980fe372881b549b0ca6005
ivanzufabc <55167874+ivanzufabc@users.noreply.github.com>
2025-07-31 10:11:15 -0300

    Add files via upload

---
+++ VendMachine.vhdl
@@ -0,0 +1,156 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+entity VendMachine is
+    port (
+        clock       : in  std_logic;
+        reset       : in  std_logic;
+        button      : in  std_logic;
+        nickel_in   : in  std_logic;
+        dime_in     : in  std_logic;
+        quarter_in  : in  std_logic;
+        nickel_out  : out std_logic;
+        dime_out    : out std_logic;
+        candy_out   : out std_logic;
+        acc         : out unsigned(3 downto 0)
+    );
+end VendMachine;
+
+architecture behavioral of VendMachine is
+
+    type t_state is (
+        S_ACUMULANDO, S_DISPENSANDO_DOCE, S_DISPENSANDO_DOCE_E_NICKEL,
+        S_DISPENSANDO_DOCE_E_DIME, S_DEVOLVENDO_NICKEL, S_DEVOLVENDO_DIME
+    );
+    signal present_state, next_state : t_state;
+
+    signal s_valor_acumulado : unsigned(3 downto 0) := (others => '0');
+    signal s_proximo_valor   : unsigned(3 downto 0);
+    signal s_troco           : unsigned(3 downto 0);
+
+    signal tmp               : std_logic := '0';
+
+    -- Função de conversão para display 7 segmentos
+    function to7seg(input: integer range 0 to 9) return std_logic_vector is
+        variable seg : std_logic_vector(6 downto 0);
+    begin
+        case input is
+            when 0 => seg := "1000000"; when 1 => seg := "1111001";
+            when 2 => seg := "0100100"; when 3 => seg := "0110000";
+            when 4 => seg := "0011001"; when 5 => seg := "0010010";
+            when 6 => seg := "0000010"; when 7 => seg := "1111000";
+            when 8 => seg := "0000000"; when 9 => seg := "0010000";
+            when others => seg := "1111111";
+        end case;
+        return seg;
+    end function;
+
+begin
+    
+    acc <= s_valor_acumulado;
+
+    -- Processo de clock e reset
+    process(reset, clock)
+    begin
+        if reset = '1' then
+            present_state     <= S_ACUMULANDO;
+            s_valor_acumulado <= (others => '0');
+            tmp <= '0';
+        elsif rising_edge(clock) then
+            present_state     <= next_state;
+            s_valor_acumulado <= s_proximo_valor;
+            tmp <= button;
+        end if;
+    end process;
+
+    -- FSM: lógica combinacional
+    process (present_state, s_valor_acumulado, nickel_in, dime_in, quarter_in, button, tmp)
+    begin
+
+        case present_state is
+            when S_ACUMULANDO =>
+                nickel_out      <= '0';
+                dime_out        <= '0';
+                candy_out       <= '0';
+                if to_integer(s_valor_acumulado) = 5 then
+                    next_state      <= S_DISPENSANDO_DOCE;
+                    s_proximo_valor <= (others => '0');
+                elsif to_integer(s_valor_acumulado) = 6 then
+                    next_state      <= S_DISPENSANDO_DOCE_E_NICKEL;
+                    s_proximo_valor <= (others => '0');
+                elsif to_integer(s_valor_acumulado) = 7 then
+                    next_state      <= S_DISPENSANDO_DOCE_E_DIME;
+                    s_proximo_valor <= (others => '0');
+                elsif to_integer(s_valor_acumulado) = 8 then
+                    next_state      <= S_DEVOLVENDO_NICKEL;
+                    s_proximo_valor <= to_unsigned(7, 4);
+                elsif to_integer(s_valor_acumulado) = 9 then
+                    next_state      <= S_DEVOLVENDO_DIME;
+                    s_proximo_valor <= to_unsigned(7, 4);
+                elsif ((not tmp) and button) = '1' then
+                    next_state <= S_ACUMULANDO;
+                    if nickel_in = '1' then
+                        s_proximo_valor <= s_valor_acumulado + 1;
+                    elsif dime_in = '1' then
+                        s_proximo_valor <= s_valor_acumulado + 2;
+                    elsif quarter_in = '1' then
+                        s_proximo_valor <= s_valor_acumulado + 5;
+                    else
+                        s_proximo_valor <= s_valor_acumulado;
+                    end if;
+                else
+                    next_state      <= S_ACUMULANDO;
+                    s_proximo_valor <= s_valor_acumulado;
+                end if;
+
+            when S_DISPENSANDO_DOCE =>
+                nickel_out      <= '0';
+                dime_out        <= '0';
+                candy_out       <= '1';
+                s_proximo_valor <= s_valor_acumulado;
+                next_state      <= S_ACUMULANDO;
+
+            when S_DISPENSANDO_DOCE_E_NICKEL =>
+                nickel_out      <= '1';
+                dime_out        <= '0';
+                candy_out       <= '1';
+                s_proximo_valor <= s_valor_acumulado;
+                next_state      <= S_ACUMULANDO;
+
+            when S_DISPENSANDO_DOCE_E_DIME =>
+                nickel_out      <= '0';
+                dime_out        <= '1';
+                candy_out       <= '1';
+                s_proximo_valor <= s_valor_acumulado;
+                next_state      <= S_ACUMULANDO;
+
+            when S_DEVOLVENDO_NICKEL =>
+                nickel_out      <= '1';
+                dime_out        <= '0';
+                candy_out       <= '0';
+                s_proximo_valor <= s_valor_acumulado;
+                next_state      <= S_ACUMULANDO;
+
+            when S_DEVOLVENDO_DIME =>
+                nickel_out      <= '0';
+                dime_out        <= '1';
+                candy_out       <= '0';
+                s_proximo_valor <= s_valor_acumulado;
+                next_state      <= S_ACUMULANDO;
+
+            when others =>
+                nickel_out      <= '0';
+                dime_out        <= '0';
+                candy_out       <= '0';
+                s_proximo_valor <= s_valor_acumulado;
+                next_state      <= present_state;
+        end case;
+    end process;
+
+    -- Lógica do Display de Troco
+--    s_troco <= to_unsigned(5, 7)  when s_nickel_out = '1' else
+--               to_unsigned(10, 7) when s_dime_out   = '1' else
+--               (others => '0');
+
+end behavioral;
---
+++ top.vhdl
@@ -0,0 +1,91 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+entity top is
+    port (
+        CLOCK_50 : in  std_logic;
+        KEY      : in  std_logic_vector(1 downto 0); -- KEY(1) = reset
+        LEDS     : out std_logic_vector(5 downto 0)
+    );
+end top;
+
+architecture behavioral of top is
+    component VendMachine is
+    port (
+        clock       : in  std_logic;
+        reset       : in  std_logic;
+        button      : in  std_logic;
+        nickel_in   : in  std_logic;
+        dime_in     : in  std_logic;
+        quarter_in  : in  std_logic;
+        nickel_out  : out std_logic;
+        dime_out    : out std_logic;
+        candy_out   : out std_logic;
+        acc         : out unsigned(3 downto 0)
+    );
+    end component VendMachine;
+
+    signal HEX0     :  std_logic_vector(6 downto 0);
+    signal HEX1     :  std_logic_vector(6 downto 0);
+    signal HEX2     :  std_logic_vector(6 downto 0);
+    signal HEX3     :  std_logic_vector(6 downto 0);
+    signal HEX4     :  std_logic_vector(6 downto 0);
+    signal HEX5     :  std_logic_vector(6 downto 0);
+    signal SW       :  std_logic_vector(2 downto 0) := "010"; -- SW(0)=5¢, SW(1)=10¢, SW(2)=25¢
+
+    signal one : std_logic := '1';
+    signal zero : std_logic := '0';
+
+    signal vm_clock     : std_logic := '0';
+    signal vm_reset     : std_logic;
+    signal vm_button    : std_logic;
+    signal vm_leds      : std_logic_vector(5 downto 0);
+    signal acc          : unsigned(3 downto 0);
+
+    signal count             : integer range 0 to 50000000 := 0;
+begin
+
+    -- Divisor de clock para gerar 1Hz
+    process(CLOCK_50)
+    begin
+        if rising_edge(CLOCK_50) then
+            if count = 50000000 - 1 then
+                count <= 0;
+                vm_clock <= not vm_clock;
+            else
+                count <= count + 1;
+            end if;
+        end if;
+    end process;
+
+    vm : component VendMachine
+    port map (
+        clock       => vm_clock,
+        reset       => KEY(1),
+        button      => KEY(0),
+        nickel_in   => zero,
+        dime_in     => one,
+        quarter_in  => zero,
+        nickel_out  => vm_leds(0),
+        dime_out    => vm_leds(1),
+        candy_out   => vm_leds(2),
+        acc         => acc
+    );
+    LEDS <= not vm_leds;
+    vm_leds(5 downto 3) <= "000";
+
+    -- Displays
+    HEX5 <= "1111001" when vm_leds(2) = '1' else "1000000"; -- 1 para candy, 0 para nada
+    HEX4 <= "1111111"; -- apagado
+    HEX3 <= "1000000" when vm_leds(0) = '1' else
+            "1111001";
+    HEX2 <= "0010010" when vm_leds(0) = '1' else
+            "1000000";
+    HEX1 <= "0011001" when acc(3 downto 1) = 4 else
+            "0110000" when acc(3 downto 1) = 3 else
+            "0100100" when acc(3 downto 1) = 2 else
+            "1111001" when acc(3 downto 1) = 1 else
+            "1000000";
+    HEX0 <= "0010010" when acc(0) = '1' else "1000000";
+end behavioral;
